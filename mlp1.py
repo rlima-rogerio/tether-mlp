be6d0790cc863fd488caa195d1d1771fb0271e4a
Rogerio Lima <rlima.rogerio@gmail.com>
2022-04-20 09:53:51 -0400

    accuracy 0.75

--- mlp1.py
+++ mlp1.py
@@ -13,13 +13,14 @@ df = pd.read_csv('data/data_perm.txt', sep=" ", header=None,
                 names=["category","az_d","el_d", "az_t", "el_t", "length","tension"])
 
 y = df["category"].to_numpy()
-x = df[["az_d","el_d", "az_t", "el_t", "length","tension"]].to_numpy()
+# x = df[["az_d", "el_d", "az_t", "el_t", "tension"]].to_numpy()
+x = df[["az_d", "el_d", "az_t", "el_t", "length", "tension"]].to_numpy()
 
 # Cast the records into float values
 x = x.astype('float32')
 x = x.astype('int')
 
-X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.30, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.70, random_state=42)
 
 print("Feature matrix:", X_train.shape)
 print("Target matrix:", X_test.shape)
@@ -41,24 +42,27 @@ model = Sequential([
 	# Flatten(input_shape=(28, 28)),
 	
 	# dense layer 1
-	Dense(256, activation='sigmoid'),
+	Dense(256, activation='relu'),
 	
 	# dense layer 2
-	Dense(128, activation='sigmoid'),
+	Dense(128, activation='relu'),
 
   	# dense layer 3
-	Dense(128, activation='sigmoid'),
+	# Dense(128, activation='relu'),
+
+	# dense layer 4
+	# Dense(64, activation='relu'),
 	
 	# output layer
-	Dense(10, activation='sigmoid'),
+	Dense(3, activation='softmax'),
 ])
 
 model.compile(optimizer='adam',
 			loss='sparse_categorical_crossentropy',
 			metrics=['accuracy'])
 
-model.fit(X_train, y_train, epochs=20,
-		batch_size=2000,
+model.fit(X_train, y_train, epochs=500,
+		batch_size=1000,
 		validation_split=0.2)
 
 results = model.evaluate(X_test, y_test, verbose = 0)
